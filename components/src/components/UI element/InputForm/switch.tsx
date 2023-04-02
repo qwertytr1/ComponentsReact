86e37382f608807dad63a37a19760313ad974c97
qwertytr1 <makhnach20040208@gmail.com>
2023-04-02 18:24:16 +0300

    feat:update switcher

--- components/src/components/UI element/InputForm/switch.tsx
+++ components/src/components/UI element/InputForm/switch.tsx
@@ -1,32 +1,28 @@
-import React, { RefObject } from "react";
+import React, { RefObject, useState } from 'react';
+import { FieldErrors, FieldValues, UseFormRegister } from 'react-hook-form';
 import styles from './switch.module.css'
 type SwitchBox = {
     label: string;
     type?: string;
-    name: string;
-    reference: RefObject<HTMLInputElement>;
-    error: string;
+    field: string;
+    register: UseFormRegister<FieldValues>;
+    error: FieldErrors<FieldValues>;
     options: string[];
 }
-class Switcher extends React.Component<SwitchBox>{
-    state = { isChecked: true };
-    getInputClasses = () => {
-        return 'form-check-input' + (this.props.error ? ' is-invalid' : '');
-    };
-    render(): React.ReactNode {
-        return (
-            <div className={styles.switcher}>
-                <span>{this.props.options[0]}</span>
-                <input className={styles.checkbox} type="checkbox" name={this.props.name} ref={this.props.reference} />
-                <span>{this.props.options[1]}</span>
-                {this.props.label && (
-                    <label className="form-checkLabel" data-htmlfor={this.props.name}>
-                        {this.props.label}
-                    </label>
-                )}
-                {<div className="error">{this.props.error}</div>}
-            </div>
-        )
-    }
+function Switcher({ label, field, error, register, options }: SwitchBox) {
+    return (
+        <div className={styles.switcher}>
+            <span>{options[0]}</span>
+            <input className={styles.checkbox} type="checkbox" data-testid="gender" {...register(field)} />
+            <span>{options[1]}</span>
+            {label && (
+                <label className="form-checkLabel" htmlFor={field}>
+                    {label}
+                </label>
+            )}
+            {<div className="error">{error[field]?.message?.toString()}</div>}
+        </div>
+
+    )
 }
 export default Switcher;
